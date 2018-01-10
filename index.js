ccf0686d7a118b1087b34183ab615470d7cefe56
pavanery <pavanery@gmail.com>
2018-01-09 22:51:39 -0300

    quick start from the facebook getting started

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 'use strict';
 
+//https://developers.facebook.com/docs/messenger-platform/getting-started/quick-start
+
 // Imports dependencies and set up http server
 const 
   express = require('express'),
   bodyParser = require('body-parser'),
   app = express().use(bodyParser.json()); // creates express http server
 
+const PAGE_ACCESS_TOKEN = process.env.FB_PAGE_ACCESS_TOKEN
+
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
+
+// Index route
+app.get('/', function (req, res) {
+	res.send('Hello world, I am a FB chat bot. See: https://developers.facebook.com/docs/messenger-platform/getting-started/quick-start')
+})
 
 // Creates the endpoint for our webhook 
 app.post('/webhook', (req, res) => {  
@@ -24,6 +33,18 @@ app.post('/webhook', (req, res) => {
         // will only ever contain one message, so we get index 0
         let webhookEvent = entry.messaging[0];
         console.log(webhookEvent);
+
+        // Get the sender PSID
+        let senderPsid = webhookEvent.sender.id;
+        console.log('Sender PSID: ' + senderPsid);
+
+         // Check if the event is a message or postback and
+        // pass the event to the appropriate handler function
+        if (webhookEvent.message) {
+          handleMessage(senderPsid, webhookEvent.message);        
+        } else if (webhookEvent.postback) {
+          handlePostback(senderPsid, webhookEvent.postback);
+        }
       });
   
       // Returns a '200 OK' response to all requests
@@ -62,3 +83,87 @@ app.get('/webhook', (req, res) => {
       }
     }
   });
+
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
+  let response;
+  // Checks if the message contains text
+  if (received_message.text) {    
+    // Create the payload for a basic text message, which
+    // will be added to the body of our request to the Send API
+    response = {
+      "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
+    }
+  } else if (received_message.attachments) {
+    // Get the URL of the message attachment
+    let attachment_url = received_message.attachments[0].payload.url;
+    response = {
+      "attachment": {
+        "type": "template",
+        "payload": {
+          "template_type": "generic",
+          "elements": [{
+            "title": "Is this the right picture?",
+            "subtitle": "Tap a button to answer.",
+            "image_url": attachment_url,
+            "buttons": [
+              {
+                "type": "postback",
+                "title": "Yes!",
+                "payload": "yes",
+              },
+              {
+                "type": "postback",
+                "title": "No!",
+                "payload": "no",
+              }
+            ],
+          }]
+        }
+      }
+    }
+  } 
+  
+  // Send the response message
+  callSendAPI(sender_psid, response);    
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+  let response;
+  // Get the payload for the postback
+  let payload = received_postback.payload;
+  // Set the response based on the postback payload
+  if (payload === 'yes') {
+    response = { "text": "Thanks!" }
+  } else if (payload === 'no') {
+    response = { "text": "Oops, try sending another image." }
+  }
+  // Send the message to acknowledge the postback
+  callSendAPI(sender_psid, response);
+}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+  // Construct the message body
+  let request_body = {
+    "recipient": {
+      "id": sender_psid
+    },
+    "message": response
+  }
+
+  // Send the HTTP request to the Messenger Platform
+  request({
+    "uri": "https://graph.facebook.com/v2.6/me/messages",
+    "qs": { "access_token": PAGE_ACCESS_TOKEN },
+    "method": "POST",
+    "json": request_body
+  }, (err, res, body) => {
+    if (!err) {
+      console.log('message sent!')
+    } else {
+      console.error("Unable to send message:" + err);
+    }
+  }); 
+}
