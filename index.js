62f41f398058b9d907baecc2fb183286206d545e
pavanery <pavanery@gmail.com>
2018-01-10 00:21:28 -0300

    add log to post function

--- index.js
+++ index.js
@@ -31,13 +31,12 @@ app.get('/', function (req, res) {
 app.post('/webhook', (req, res) => {  
  
     let body = req.body;
-  
+    console.log('[DEBUG][post::forEach]', body);
+
     // Checks this is an event from a page subscription
     if (body.object === 'page') {
-  
       // Iterates over each entry - there may be multiple if batched
       body.entry.forEach(function(entry) {
-  
         // Gets the message. entry.messaging is an array, but 
         // will only ever contain one message, so we get index 0
         let webhookEvent = entry.messaging[0];
